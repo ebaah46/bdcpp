1386ea6d2883c09aa564031e2b4e3f25527a3193
ebaah <ebaah46@yahoo.com>
2022-08-25 19:11:00 +0300

    cmake trials

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-#include <detector_algo.h>
-#include <fmtmsg.h>
+#include <GLFW/glfw3.h>
+#include <bd_config.h>
+#ifdef USE_D
+    #include <detector_algo.h>
+#endif
+int main(int argc, char*argv[]) {
+    std::cout << "Hello, Squad!" << std::endl;
+    #ifdef USE_D
+        detector::Detector_Algo::doNothing();
+        std::cout<<argv[0] <<"Version "<<bottle_detector_VERSION_MAJOR<<"."<<bottle_detector_VERSION_MINOR<<std::endl;
+    #endif
+    GLFWwindow * window;
+    if(!glfwInit()){
+        fprintf(stderr,"Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+    window = glfwCreateWindow(400,400,"Detector",NULL,NULL);
+    if(!window){
+        fprintf(stderr,"Failed to open GLFW window");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    while(!glfwWindowShouldClose(window)){
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    detector::Detector_Algo::doNothing();
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+
+    // Exit program
+    exit( EXIT_SUCCESS );
     return 0;
 }
